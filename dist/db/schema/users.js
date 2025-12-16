4f701925f70f905fd78fc8d36f1ed9aa25b9263b
Andrew <andrew.publik.mail@gmail.com>
2025-12-16 12:21:07 +0300

    Stop tracking dist

--- dist/db/schema/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.users = void 0;
-const mysql_core_1 = require("drizzle-orm/mysql-core");
-exports.users = (0, mysql_core_1.mysqlTable)('users', {
-    userId: (0, mysql_core_1.varchar)('user_id', { length: 50 }).primaryKey(),
-    passwordHash: (0, mysql_core_1.varchar)('password_hash', { length: 255 }).notNull(),
-    createdAt: (0, mysql_core_1.timestamp)('created_at').defaultNow()
-});
